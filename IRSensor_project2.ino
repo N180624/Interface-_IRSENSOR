8a4e34ccd753ada6c60f29db16d7aa4bf8425ca2
N180624 <109448089+N180624@users.noreply.github.com>
2022-07-30 12:11:20 +0100

    Add files via upload
     INTERFACE_IRSENSOR

---
+++ IRSensor_project2.ino
@@ -0,0 +1,20 @@
+void setup()
+{
+  pinMode(13,OUTPUT);
+  pinMode(5,INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if (digitalRead(5)==LOW)
+  {
+    digitalWrite(13,HIGH);
+    delay(10);
+  }
+  else
+  {
+    digitalWrite(13,LOW);
+    delay(10);
+  }
+}
